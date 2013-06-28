a89b733e11b358a4d6dd88986eab5d7401ee1320
Jascha Sohl-Dickstein <jascha.sohldickstein@gmail.com>
2013-06-28 10:14:49 -0700

    added mirt file

--- hive/shell_commands.sh
+++ hive/shell_commands.sh
@@ -10,7 +10,7 @@ analytics/src/emr.py 's3://ka-mapreduce/tmp/scripts_combined.q'
 # ssh -L 2020:[lookup ip from above]:9100 ka-analytics
 # connect to http://localhost:2020 to check status
 
-# after query finishes
+# after query finishes, still on ka-analytics
 cd /ebs/kadata/tmp/jascha/
 rm -rf gm_ab_perproblem
 s3cmd get --recursive s3://ka-mapreduce/tmp/jascha/gm_ab_perproblem
