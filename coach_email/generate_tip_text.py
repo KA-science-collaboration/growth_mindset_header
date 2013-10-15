17e1216225e1e90122d32507c6c6a085af87571e
Jascha Sohl-Dickstein <jascha.sohldickstein@gmail.com>
2013-10-15 15:24:26 -0700

    debugging

--- coach_email/generate_tip_text.py
+++ coach_email/generate_tip_text.py
@@ -6,9 +6,38 @@ import event_log
 
 
 
+# TODO(jascha) site tip could probably use some work
+
+# define the tip strings
+tip_strings = {
+    'no tip': None,
+    'site tip': 'Tip: You can find more information about your students and what they are struggling with by visiting the <a href="https://www.khanacademy.org/coach">coach dashboard</a>.',
+    'positive encouragment': 'Science Tip: Recent research shows that students learn more when they are confident they can succeed at the problems they practice. When they have faith in themselves or know that others believe they can be successful, it helps them to be more motivated and not get discouraged. Help your students be confident that they are up to the task, and that you know they can do it.',
+    'growth neuroscience tip': 'Science Tip: Recent research shows that students learn more after being taught <em>"Your brain is like a muscle. The more you exercise it, the stronger and smarter it gets"</em>. Let your students know how they made their brains stronger this week!',
+    'growth effort tip': 'Science Tip:  Recent research shows that students learn more when they are praised for their effort and hard work.  Students learn less when they are praised for being smart.  Let your students know how hard they worked this week.',
+    'growth intelligence tip': 'Science Tip: Recent research shows that students learn more when they are taught that the harder they think the smarter they get.  Students do worse if they believe that intelligence is fixed, and that no matter how hard they try they will stay as smart or dumb as they feel right now.  Let your students know how much smarter they got by thinking hard this week.',
+    'growth malleable': 'Science Tip: Recent research shows that students learn more when they are taught that intelligence is malleable and can be grown through hard work. Students do worse if they believe that intelligence is fixed, and so are afraid to make mistakes or ask questions because they will look like they aren\'t smart.  Let your students know that they\'re getting smarter every minute they spend practicing math and tackling tough problems.',
+    'growth malleable link': 'Science Tip: Recent research shows that students learn more when they are taught that intelligence is malleable and can be grown through hard work. Students do worse if they believe that intelligence is fixed, and so are afraid to make mistakes or ask questions because they will look like they aren\'t smart.  Let your students know that they\'re getting smarter every minute they spend practicing math and tackling tough problems.  To teach your students more, you can show them <a href="https://www.khanacademy.org/brainworkout_1">this page</a>.',
+    'growth malleable nonexpert': 'Tip: Students learn more when they are taught that intelligence is malleable and can be grown through hard work. Students do worse if they believe that intelligence is fixed, and so are afraid to make mistakes or ask questions because they will look like they aren\'t smart.  Let your students know that they\'re getting smarter every minute they spend practicing math and tackling tough problems.',
+    'explanation short silent': 'Science Tip: Recent research shows that students learn more when they are taught to pause and explain out loud why they are doing each step in a math problem.  Students do worse when they develop a habit of trying things at random when they get frustrated or confused.  Let your students know that they can learn more by pausing and explaining aloud.',
+    'explanation short aloud': 'Science Tip: Recent research shows that students learn more when they are taught to pause and explain out loud why they are doing each step in a math problem.  Students do worse when they develop a habit of trying things at random when they get frustrated or confused.  Let your students know that they can learn more by pausing and explaining aloud.',
+    'explanation long aloud': 'Science Tip: Recent research shows that students learn more when they explain their thinking while they are doing the steps of a math problem. Let your students know that if they get stuck, they can help themselves by explaining out loud what they are thinking, or trying to explain their thinking to another person. You can also try more frequently asking your students to provide explanations, letting them know that you\'re not trying to test them, but to help them learn.',
+    }
+# make each tip bold, and with emphasis at the beginning if appropriate
+for tip_key in tip_strings.keys():
+    tip_text = tip_strings[tip_key]
+    if tip_text is None:
+        continue
+    chunks = tip_text.split(':', 1)
+    if len(chunks) > 1:
+        chunks[0] = '<strong>' + chunks[0] + ':' + '</strong>'
+        tip_text = chunks[0] + chunks[1]
+        tip_strings[tip_key] = tip_text
+
+
 def hash_to_index(max_length, salt=0):
     """
-    Generate a pseudorandom integer less than max_length, using a hash function
+    Generate a pseudorandom integer >= 0 and < max_length, using a hash function
     so it's reproducible.  The hash function is based on the current week, the
     current user, and a salt input.  For the same user, salt, and max_length the
     random number will only change once per week, at midnight Thursday night/
@@ -36,47 +65,16 @@ def hash_to_index(max_length, salt=0):
     return sig_index
 
 
-# all weights are given in terms of percentages
-
-# TODO(jascha) is 'coach' the core category we want?
-# TODO(jascha) site tip could probably use some work
-# TODO(jascha) write something for behavior specific praise condition
-
-tip_strings = {
-    'no tip': None,
-    'site tip': 'Tip: You can find more information about your students and what they are struggling with by visiting the <a href="https://www.khanacademy.org/coach">coach dashboard</a>.',
-    'positive encouragment': 'Science Tip: Recent research shows that students learn more when they are confident they can succeed at the problems they practice. When they have faith in themselves or know that others believe they can be successful, it helps them to be more motivated and not get discouraged. Help your students be confident that they are up to the task, and that you know they can do it.',
-    'behavior specific praise': '',
-    'growth neuroscience tip': 'Science Tip: Recent research shows that students learn more after being taught <strong>"Your brain is like a muscle. The more you exercise it, the stronger and smarter it gets"</strong>. Let your students know how they made their brains stronger this week!',
-    'growth effort tip': 'Science Tip:  Recent research shows that students learn more when they are praised for their effort and hard work.  Students learn less when they are praised for being smart.  Let your students know how hard they worked this week.',
-    'growth intelligence tip': 'Science Tip: Recent research shows that students learn more when they are taught that the harder they think the smarter they get.  Students do worse if they believe that intelligence is fixed, and that no matter how hard they try they will stay as smart or dumb as they feel right now.  Let your students know how much smarter they got by thinking hard this week.',
-    'growth malleable': 'Science Tip: Recent research shows that students learn more when they are taught that intelligence is malleable and can be grown through hard work. Students do worse if they believe that intelligence is fixed, and so are afraid to make mistakes or ask questions because they will look like they aren’t smart.  Let your students know that they’re getting smarter every minute they spend practicing math and tackling tough problems.',
-    'growth malleable link': 'Science Tip: Recent research shows that students learn more when they are taught that intelligence is malleable and can be grown through hard work. Students do worse if they believe that intelligence is fixed, and so are afraid to make mistakes or ask questions because they will look like they aren’t smart.  Let your students know that they’re getting smarter every minute they spend practicing math and tackling tough problems.  To teach your students more, you can show them <a href="https://www.khanacademy.org/brainworkout_1">this page</a>.',
-    'growth malleable nonexpert': 'Tip: Students learn more when they are taught that intelligence is malleable and can be grown through hard work. Students do worse if they believe that intelligence is fixed, and so are afraid to make mistakes or ask questions because they will look like they aren’t smart.  Let your students know that they’re getting smarter every minute they spend practicing math and tackling tough problems.',
-    'explanation short silent': 'Science Tip: Recent research shows that students learn more when they are taught to pause and explain out loud why they are doing each step in a math problem.  Students do worse when they develop a habit of trying things at random when they get frustrated or confused.  Let your students know that they can learn more by pausing and explaining aloud.',
-    'explanation short aloud': 'Science Tip: Recent research shows that students learn more when they are taught to pause and explain out loud why they are doing each step in a math problem.  Students do worse when they develop a habit of trying things at random when they get frustrated or confused.  Let your students know that they can learn more by pausing and explaining aloud.',
-    'explanation long aloud': 'Science Tip: Recent research shows that students learn more when they explain their thinking while they are doing the steps of a math problem. Let your students know that if they get stuck, they can help themselves by explaining out loud what they are thinking, or trying to explain their thinking to another person. You can also try more frequently asking your students to provide explanations, letting them know that you’re not trying to test them, but to help them learn.',
-    }
-
-# make each tip bold, and with emphasis at the beginning if appropriate
-for tip_key in tip_strings.keys():
-    tip_text = tip_strings[tip_key]
-    if tip_text is None:
-        continue
-    chunks = tip_text.split(':', 1)
-    if len(chunks) > 1:
-        chunks[0] = '<em>' + chunks[0] + ':' + '</em>'
-    tip_text = chunks[0] + chunks[1]
-    tip_text = '<b>' + tip_text + '</b>'
-    tip_strings[tip_key] = tip_text
-
 def get_tip_string():
     """
     Generate the tip string to be used for the intervention experiments in the
     coach email.
     """
 
+    # all A/B test weights are given as percentages
+
     # get A/B test condition
+    # TODO(jascha) is 'coach' the core category we want?
     test_condition_top = experiments.CoreMetrics.ab_test(
         'coach email intervention',
         alternative_params={
@@ -86,22 +84,20 @@ def get_tip_string():
             'combination condition': 15
             },
         core_categories='coach')
-
     test_condition_text = test_condition_top
 
     if test_condition_top == 'control':
         test_condition_control = experiments.CoreMetrics.ab_test(
             'coach email intervention - control subtest',
             alternative_params={
-                'no tip': 25,
+                'no tip': 30,
                 'site tip': 5,
                 'positive encouragment': 5,
-                'behavior specific praise': 5
                 },
             core_categories='coach')
         test_condition_text += '.' + test_condition_control
         tip_text = tip_strings[test_condition_control]
-    elif test_condition_top == 'growth mindset tip':
+    elif test_condition_top == 'growth mindset':
         test_condition_growth = experiments.CoreMetrics.ab_test(
             'coach email intervention - growth subtest',
             alternative_params={
@@ -148,8 +144,10 @@ def get_tip_string():
         test_condition_text += '.' + str(sig_index)
         tip_text = tip_strings[tip_strings.keys()[sig_index]]
 
-    # only show a message at all with 50% probability
-    if hash_to_index(max_length, salt='display') == 0:
+    # only show a message at all with 50% probability to allow comparisons of
+    # on weeks and off weeks
+    if hash_to_index(2, salt='display') == 0:
+        test_condition_text += '.no display'
         tip_text = None
 
     # store the tip text in the KA logs
